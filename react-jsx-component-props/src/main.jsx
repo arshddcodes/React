5676a3f1104ee6ad2a292985c392105c748cd096
arshddcodes <arshadkallankodan@gmail.com>
2025-08-11 22:27:12 +0530

    react jsx,component,props

---
+++ react-jsx-component-props/src/main.jsx
@@ -0,0 +1,38 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+
+// first task variable
+const name = "arshad";
+const num = 10;
+
+// second task variable
+const fname = "arshad";
+const lname = "kk";
+
+createRoot(document.getElementById("root"))
+  .render
+  // first task\
+
+  // <div>
+  // <h1>hello {name}</h1>
+  // <h1>My Number is {num}</h1>
+  // </div>
+
+  // second task
+
+  // <div>
+  //   <h1>hello {fname + " " + lname}</h1>
+  //   <h1>hello {fname} {lastname}</h1>
+  //   <h1>hello {`${fname} ${lname} `}</h1>
+  // </div>
+
+  // third task
+  
+
+  ();
+
+// <StrictMode>
+//   <App />
+// </StrictMode>,
